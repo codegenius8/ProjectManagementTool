7a91f5df950f5a25cc80439596e8b847754c22cd
codegenius8 <94888474+codegenius8@users.noreply.github.com>
2023-01-04 12:48:57 +0530

    Update BoardWrapperContainer.js

--- client/src/containers/BoardWrapperContainer.js
+++ client/src/containers/BoardWrapperContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 
 import { currentBoardSelector } from '../selectors';
 import BoardWrapper from '../components/BoardWrapper';
-
+//currentBoardSelector
 const mapStateToProps = (state) => {
   const { type, isFetching } = currentBoardSelector(state);
 
